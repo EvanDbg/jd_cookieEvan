bcef4b8528ecfb01422584298c9fc124df76aebc
EvanDbg <yangf1023@icloud.com>
2021-10-09 07:44:44 +0800

    Update submit.go

--- submit.go
+++ submit.go
@@ -18,7 +18,33 @@ var pin = func(class string) core.Bucket {
 }
 
 func init() {
-	//
+	// 
+	core.Server.POST("/send_private_msg", func(c *gin.Context) {
+		user_id := c.Query("user_id")
+		access_token := c.Query("access_token")
+		message := c.PostForm("message")
+		type Result struct {
+			Code    int         `json:"retCode"`
+			Data    interface{} `json:"data"`
+			Message string      `json:"message"`
+			ErrMsg  string      `json:"errmsg"`
+		}
+		result := Result{
+			Data: nil,
+			Code: 0,
+		}
+		notify_token_cfg := jd_cookie.Get("access_token")
+		if access_token != notify_token_cfg {
+			result.ErrMsg = "notify_token未设置或设置不正确。"
+			result.Code = 100
+			c.JSON(200, result)
+			return
+		}
+		core.Push("qq", core.Int(user_id), message)
+		result.Message = "发送给QQ[" + user_id + "] : " + message // "一句mmp，不知当讲不当讲。"
+		c.JSON(200, result)
+		return
+	})
 	// 可改造成发送通知的机器人
 	core.Server.POST("/notify", func(c *gin.Context) {
 		msg := c.PostForm("msg")
