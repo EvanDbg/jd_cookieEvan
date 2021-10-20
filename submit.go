1e028b4a3dd090dbb0802944fc37945963b947ba
EvanDbg <yangf1023@icloud.com>
2021-10-20 23:24:41 +0800

    Update submit.go

--- submit.go
+++ submit.go
@@ -114,7 +114,7 @@ func init() {
 		
 		json := make(map[string]interface{})
 		c.BindJSON(&json)
-		message := fmt.Println("string", json["message"].(string))
+		message := fmt.Sprintf("%s", json["message"]) // fmt.Println("string", json["message"].(string))
 		
 		type Result struct {
 			Code    int         `json:"retcode"`
